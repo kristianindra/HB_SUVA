d2d5d3dbdc9d6499bdd9699d79880558980aef29
MichaelChrisdian <michael.chrisdian@yahoo.com>
2016-05-04 21:44:08 +0700

    Added files via upload

---
+++ application/views/link.php
@@ -0,0 +1,14 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>SUVA</title>
+<link rel="stylesheet" href="asset/font-awesome/css/font-awesome.css" />
+<link rel="stylesheet" href="asset/bootstrap/css/bootstrap.min.css" />
+<script type="text/javascript" src="asset/js/jquery-1.12.2.min.js"></script>
+<link rel="stylesheet" type="text/css" href="asset/datatable/jquery.dataTables.css">
+<link rel="stylesheet" type="text/css" href="asset/datatable/dataTables.tableTools.css">
+<script type="text/javascript" charset="utf8" src="asset/datatable/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" charset="utf8" src="asset/datatable/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf8" src="asset/datatable/dataTables.tableTools.min.js"></script>
+<script type="text/javascript" src="asset/sweetalert-master/dist/sweetalert.min.js"></script>
+<link rel="stylesheet" type="text/css" href="asset/sweetalert-master/dist/sweetalert.css">
+<link rel="stylesheet" type="text/css" href="asset/css/style.css">
---
+++ application/views/v_login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include 'link.php'; ?>
+</head>
+<body>
+	<div class="container">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4" style="margin-top: 50px;" id="form">
+			<div id="head" style="text-align:center; opacity: 1;">
+				<img src="./asset/image/maxel_logo_2.PNG" style="max-width: 100px;"/>
+			</div>
+			<div class="body">
+			<form action="C_main/login" method="POST">
+			  	<div class="form-group">
+			    	<label class="sr-only" for="uname">User Name</label>
+				    <input type="text" class="form-control" id="uname" name="uname" placeholder="User Name">
+				</div>
+				<div class="form-group">
+			    	<label class="sr-only" for="upass">User Password</label>
+				    <input type="password" class="form-control" id="upass" name="upass" placeholder="User Password">
+				</div>
+				<button type="submit" class="btn btn-block btn-default">Log In</button>
+				<br/>
+			</form>
+			</div>
+		</div>
+		<div class="col-sm-4"></div>
+	</div>
+	<script src="asset/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
